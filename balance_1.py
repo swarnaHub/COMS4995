6327e929439da9a1aab62096971a2f7d426b5c5d
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2018-03-30 16:24:57 -0400

    Update balance_1.py

--- balance_1.py
+++ balance_1.py
@@ -3,85 +3,71 @@ import numpy
 from sklearn.model_selection import KFold
 import os
 
-#===============================================================================================================
-#Uncomment parts in between equals and run
-#Part 1
 
-# path = 'wikitalk.txt'
-# l = []
-# for line in open(path,'r'):
-# 	l.append((line.split('\t')[0],line.split('\t')[1]))
 
-# random.seed(42)
+path = 'wikitalk.txt'
+l = []
+for line in open(path,'r'):
+	l.append((line.split('\t')[0],line.split('\t')[1]))
 
-# claim = 0
-# cl = []
-# noncl = []
-# for tup in l:
-# 	if int(tup[1])==1:
-# 		claim = claim+1
-# 		cl.append(tup)
-# 	elif int(tup[1])==0:
-# 		noncl.append(tup)
-# # l = numpy.array(l)
-# # numpy.random.shuffle(l)
+random.seed(42)
 
-# cl = numpy.array(cl)
-# noncl = numpy.array(noncl)
+claim = 0
+cl = []
+noncl = []
+for tup in l:
+	if int(tup[1])==1:
+		claim = claim+1
+		cl.append(tup)
+	elif int(tup[1])==0:
+		noncl.append(tup)
+# l = numpy.array(l)
+# numpy.random.shuffle(l)
+
+cl = numpy.array(cl)
+noncl = numpy.array(noncl)
 
 
 
-# cv_object1 = KFold(n_splits=10, shuffle=True, random_state=42)
-# count = 1
-# for train_index1, test_index1 in cv_object1.split(cl):
-# 		f = open('./data/cv_'+str(count),'w')
-# 		for k in range(len(test_index1)):
-# 			f.write(cl[test_index1[k]][0]+'\t'+cl[test_index1[k]][1])
-# 		count = count+1
+cv_object1 = KFold(n_splits=10, shuffle=True, random_state=42)
+count = 1
+for train_index1, test_index1 in cv_object1.split(cl):
+		f = open('./data/cv_'+str(count),'w')
+		for k in range(len(test_index1)):
+			f.write(cl[test_index1[k]][0]+'\t'+cl[test_index1[k]][1])
+		count = count+1
 
-# cv_object2 = KFold(n_splits=10, shuffle=True, random_state=42)
-# count = 1
-# for train_index2, test_index2 in cv_object2.split(noncl):
-# 		f = open('./data/cv_'+str(count),'a')
-# 		for k in range(len(test_index2)):
-# 			f.write(noncl[test_index2[k]][0]+'\t'+noncl[test_index2[k]][1])
-# 		count = count+1
+cv_object2 = KFold(n_splits=10, shuffle=True, random_state=42)
+count = 1
+for train_index2, test_index2 in cv_object2.split(noncl):
+		f = open('./data/cv_'+str(count),'a')
+		for k in range(len(test_index2)):
+			f.write(noncl[test_index2[k]][0]+'\t'+noncl[test_index2[k]][1])
+		count = count+1
 
-#====================================================================================================================
-
-
-#===============================================================================================================
-#Uncomment parts in between equals and run
-#Part 2
-
-# a = [1,2,3,4,5,6,7,8,9,10]
-# for i in range(1,11):
-# 	x = random.sample(range(1,len(a)+1),8)
-# 	notin = []
-# 	for num in a:
-# 		if num not in x:
-# 			notin.append(num)
-# 	print x,notin
-# 	f = open('./concatdata/train/cv_'+str(i),'a')
-# 	for num in x:
-# 		f1 = open('./data/cv_'+str(num),'r')
-# 		for line in f1:
-# 			f.write(line)
-# 	f = open('./concatdata/dev/cv_'+str(i),'a')
-# 	f1 = open('./data/cv_'+str(notin[0]),'r')
-# 	for line in f1:
-# 		f.write(line)
-# 	f = open('./concatdata/test/cv_'+str(i),'a')
-# 	f1 = open('./data/cv_'+str(notin[1]),'r')
-# 	for line in f1:
-# 		f.write(line)
-
-#===============================================================================================================
+a = [1,2,3,4,5,6,7,8,9,10]
+for i in range(1,11):
+	x = random.sample(range(1,len(a)+1),8)
+	notin = []
+	for num in a:
+		if num not in x:
+			notin.append(num)
+	print x,notin
+	f = open('./concatdata/train/cv_'+str(i),'a')
+	for num in x:
+		f1 = open('./data/cv_'+str(num),'r')
+		for line in f1:
+			f.write(line)
+	f = open('./concatdata/dev/cv_'+str(i),'a')
+	f1 = open('./data/cv_'+str(notin[0]),'r')
+	for line in f1:
+		f.write(line)
+	f = open('./concatdata/test/cv_'+str(i),'a')
+	f1 = open('./data/cv_'+str(notin[1]),'r')
+	for line in f1:
+		f.write(line)
 
 
-#===============================================================================================================
-#Uncomment parts in between equals and run
-#Part 3
 
 for i in range(1,11):
 	f = open('./concatdata/train/cv_'+str(i),'r')
@@ -105,7 +91,6 @@ for i in range(1,11):
 		for line in a:
 			g.write(line)
 
-#===============================================================================================================
 
 
 
