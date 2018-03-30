d9fc306a33e554185c9a2bf2be5e805361c68148
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2018-03-30 15:11:33 -0400

    Add files via upload

---
+++ majority_pred.py
@@ -0,0 +1,30 @@
+from itertools import izip
+num = 3
+
+a = ['bilstm_pred_'+str(num)+'_'+str(j) for j in range(1,21)]
+f = open('./indomain_pred/maj_'+str(num),'w')
+print a
+
+c = []
+for i in range(0,859):
+	c.append([])
+for i in range(0,859):
+	c[i].append(0)
+	c[i].append(0)
+
+
+for i in range(len(a)):
+	count = 0
+	with open('./indomain_pred/'+a[i]) as t1:
+		for line in t1:
+			line = int(line.strip())
+			c[count][line] = c[count][line]+1
+			count = count+1
+
+print c[0:10]
+
+for i in range(len(c)):
+	if c[i][0]>c[i][1]:
+		f.write(str(0)+'\n')
+	else:
+		f.write(str(1)+'\n')
---
+++ scores.py
@@ -0,0 +1,24 @@
+
+from sklearn.metrics import classification_report , precision_recall_fscore_support,precision_score,recall_score,f1_score
+
+f = open('classification_report.txt','w')
+
+for i in range(1,4):
+	y_pred = []
+	y_test = []
+
+	for line in open('./concatdata/test/cv_'+str(i),'r'):
+		line = line.strip().split('\t')
+		y_test.append(int(line[1]))
+
+
+
+	for line in open('./indomain_pred/maj_'+str(i),'r'):
+		line = line.strip()
+		y_pred.append(int(line))
+
+
+
+	f.write(str(classification_report(y_test, y_pred)+'\n'))
+	f.write(str(f1_score(y_test, y_pred, average='macro'))+'\n')
+	f.write("-------------------------------------------------\n")
