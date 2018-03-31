2b12352cbd0a3c322299af9656e646a7a4215a78
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2018-03-31 12:05:14 -0400

    Add files via upload

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-num = 4
-for i in range(16,21):
-	train_file = './concatdata/train/train_4_20/'+str(num)+'_'+str(i)
-	test_file =  './concatdata/test/cv_'+str(num)
-	dev_file =  './concatdata/dev/cv_'+str(num)
-	command = 'KERAS_BACKEND=theano python bidirectional_lstm.py '+train_file+' '+dev_file+' '+test_file+' '+str(num)+'_'+str(i)
-	os.system(command)
+for num in range(9,11):
+	for i in range(1,21):
+		train_file = './concatdata/train/train_'+str(num)+'_20/'+str(num)+'_'+str(i)
+		test_file =  './concatdata/test/cv_'+str(num)
+		dev_file =  './concatdata/dev/cv_'+str(num)
+		command = 'KERAS_BACKEND=theano python bidirectional_lstm.py '+train_file+' '+dev_file+' '+test_file+' '+str(num)+'_'+str(i)
+		os.system(command)
