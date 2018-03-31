99964f82014fee91de5554eac88c03a16fda6bb4
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2018-03-31 12:05:27 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-for num in range(9,11):
+for num in range(1,11):
 	for i in range(1,21):
 		train_file = './concatdata/train/train_'+str(num)+'_20/'+str(num)+'_'+str(i)
 		test_file =  './concatdata/test/cv_'+str(num)
